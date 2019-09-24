7e4606a560feff0063ac9194c961cc4cfea18e5b
winmo <win.moi@hotmail.com>
2019-09-24 13:51:34 +0200

    Librairie V1 fonctionnelle

---
+++ NombreComplexe/NombreComplexe.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace NombreComplexe
+{
+    public class NombreComplexe
+    {
+        private double _reel;
+        private double _imaginaire;
+
+        public NombreComplexe(double reel, double imaginaire)
+        {
+            _reel = reel;
+            _imaginaire = imaginaire;
+        }
+
+        public void Afficher(char lettreImaginaire = 'i')
+        {
+            char signe;
+
+            if (_imaginaire < 0)
+                signe = '-';
+            else
+                signe = '+';
+            
+            Console.WriteLine("{0} {1} {2}{3}", _reel, signe, lettreImaginaire, Math.Abs(_imaginaire));
+        }
+        
+        public static NombreComplexe operator +(NombreComplexe nombre1, NombreComplexe nombre2)
+        {
+            return new NombreComplexe
+                ( nombre1._reel + nombre2._reel , nombre1._imaginaire + nombre2._imaginaire );
+        }
+        
+        public static NombreComplexe operator +(NombreComplexe nombre1, double nombre2)
+        {
+            return new NombreComplexe
+                ( nombre1._reel + nombre2 , nombre1._imaginaire);
+        }
+        
+        public static NombreComplexe operator -(NombreComplexe nombre1, NombreComplexe nombre2)
+        {
+            return new NombreComplexe
+                ( nombre1._reel - nombre2._reel , nombre1._imaginaire - nombre2._imaginaire );
+        }
+        
+        public static NombreComplexe operator -(NombreComplexe nombre1, double nombre2)
+        {
+            return new NombreComplexe
+                ( nombre1._reel - nombre2 , nombre1._imaginaire);
+        }
+        
+        public static NombreComplexe operator *(NombreComplexe nombre1, NombreComplexe nombre2)
+        {
+            double r1, r2, i1, i2;
+
+            r1 = nombre1._reel * nombre2._reel;
+            r2 = nombre1._imaginaire * nombre2._imaginaire;
+            i1 = nombre1._reel * nombre2._imaginaire;
+            i2 = nombre1._imaginaire * nombre2._reel;
+
+            r1 -= r2;
+            i1 += i2;
+            
+            return new NombreComplexe(r1, i1);
+        }
+        
+        public static NombreComplexe operator *(NombreComplexe nombre1, double nombre2)
+        {
+            return new NombreComplexe(nombre1._reel * nombre2, nombre1._imaginaire * nombre2);
+        }
+        
+        public static NombreComplexe operator /(NombreComplexe nombre1, double nombre2)
+        {
+            return new NombreComplexe(nombre1._reel / nombre2, nombre1._imaginaire / nombre2);
+        }
+
+        public void AuCarre()
+        {
+            double reelTemp = _reel;
+            _reel = _reel * _reel - _imaginaire * _imaginaire;
+            _imaginaire = 2 * reelTemp * _imaginaire;
+        }
+
+        public double ProduitConjugues()
+        {
+            return _reel * _reel + _imaginaire * _imaginaire;
+        }
+
+        public NombreComplexe Inverse()
+        {
+            return Conjugue() / ProduitConjugues();
+        }
+
+        public NombreComplexe Conjugue()
+        {
+            return new NombreComplexe(_reel, - _imaginaire);
+        }
+
+        public double Module()
+        {
+            return Math.Sqrt(_reel * _reel + _imaginaire * _imaginaire);
+        }
+
+        public double Reel => _reel;
+
+        public double Imaginaire => _imaginaire;
+    }
+}
---
+++ NombreComplexe/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace NombreComplexe
+{
+    class Program
+    {
+        static void Main()
+        {
+            NombreComplexe nombreComplexe = new NombreComplexe(2, 3);
+            
+            nombreComplexe = nombreComplexe.Conjugue();
+            Console.Write("Conjugue : ");
+            nombreComplexe.Afficher();
+            
+            nombreComplexe += nombreComplexe;
+            Console.Write("Somme complexe : ");
+            nombreComplexe.Afficher();
+            
+            nombreComplexe -= 2;
+            Console.Write("Différence nombre : ");
+            nombreComplexe.Afficher();
+            
+            nombreComplexe += 2;
+            Console.Write("Somme nombre : ");
+            nombreComplexe.Afficher();
+            
+            nombreComplexe = new NombreComplexe(2, 3);
+            NombreComplexe nombreComplexe2 = new NombreComplexe(4, 5);
+            
+            nombreComplexe2 *= nombreComplexe;
+            Console.Write("Multiplication complexe : ");
+            nombreComplexe2.Afficher();
+            
+            nombreComplexe.AuCarre();
+            Console.Write("Carré : ");
+            nombreComplexe.Afficher();
+            
+            nombreComplexe = new NombreComplexe(2, 3);
+            Console.Write("Produit conjugués : ");
+            Console.WriteLine(nombreComplexe.ProduitConjugues());
+            
+            Console.Write("Module : ");
+            Console.WriteLine(nombreComplexe.Module());
+            
+            Console.Write("Inverse : ");
+            nombreComplexe = nombreComplexe.Inverse();
+            nombreComplexe.Afficher();
+        }
+    }
+}
